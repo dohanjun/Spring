b47a2f38a2dfe3052b90ffd57c7a699634c77dc8
dohanjun <ehgkswns132@naver.com>
2025-02-14 17:56:10 +0900

    2025-02-14-03

--- boot01/src/main/java/com/yedam/app/emp/service/EmpVO.java
+++ boot01/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -6,16 +6,17 @@ import lombok.Data;
 
 @Data
 public class EmpVO {
-	private Integer employeeId;   // 사원번호
-    private String firstName;     // 이름
-    private String lastName;      // 성
-    private String email;         // 이메일 
-    private String phoneNumber;   // 휴대폰번호
-    private Date hireDate;        // 입사일
-    private String jobId;         // 업무
-    private double salary;        // 급여 
-    private double commissionPct; // 커미션
-    private int managerId;        // 상사번호
-    private int departmentId;     // 부서번호
-    
+	// employee_id => employeeId
+	private Integer employeeId;	// 사원번호
+	private String firstName; 	// 이름
+	private String lastName; 	// 성
+	private String email;		// 이메일
+	private String phoneNumber; // 연락처
+	private Date hireDate;		// 입사일자
+	private String jobId;		// 업무
+	private double salary; 		// 급여
+	private double commissionPct; // 커미션
+	private int managerId;		// 상사번호
+	private int departmentId; 	// 부서번호
+
 }
